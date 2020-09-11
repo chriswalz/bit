8b270d33f5f4040d2444f3ec4e9d692620d086c6
chriswalz <zlaw777@gmail.com>
2020-09-10 21:20:59 -0400

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # bit
 Git if it was made in 2020
+
+
+## principles 
+
+Bit should have principles of the cloud
+Bit should have principles of Google Docs
+Bit should be simple to use
+Bit should work around development workflows of 2020
+Bit must be fully compatible with Git
---
+++ main.go
@@ -0,0 +1,36 @@
+/*
+Copyright © 2020 NAME HERE <EMAIL ADDRESS>
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package main
+
+import (
+	"fmt"
+	bitcmd "github.com/chriswalz/bit/cmd"
+	"log"
+	"os"
+	"os/exec"
+)
+
+func main() {
+	argsWithoutProg := os.Args[1:]
+	log.Println(argsWithoutProg)
+	cmd := exec.Command("git", argsWithoutProg...)
+	msg, err := cmd.CombinedOutput()
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(string(msg))
+	bitcmd.Execute()
+}
