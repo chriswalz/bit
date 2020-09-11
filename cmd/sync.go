3d2ca15cb31389115ecf14d09541558676f4f634
chriswalz <zlaw777@gmail.com>
2020-09-11 00:58:58 -0400

    add save, sync, version commands

--- cmd/sync.go
+++ cmd/sync.go
@@ -19,6 +19,8 @@ import (
 	"fmt"
 	"github.com/chriswalz/bit/util"
 	"github.com/spf13/cobra"
+	"os/exec"
+	"strings"
 )
 
 // syncCmd represents the sync command
@@ -30,13 +32,17 @@ sync origin master
 sync local-branch
 `,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("sync called")
 		save("")
-		util.Runwithcolor([]string{"pull", "-r"})
-		if len(args) > 0 {
-			util.Runwithcolor(append([]string{"pull", "-r"}, args...))
+		if cloudBranchExists() {
+			util.Runwithcolor([]string{"pull", "-r"})
+			if len(args) > 0 {
+				util.Runwithcolor(append([]string{"pull", "-r"}, args...))
+			}
+			util.Runwithcolor([]string{"push"})
+		} else {
+			util.Runwithcolor([]string{"push", "--set-upstream", "origin", currentBranch()})
 		}
-		util.Runwithcolor([]string{"push"})
+
 	},
 	//Args: cobra.MaximumNArgs(1),
 }
@@ -45,4 +51,22 @@ func init() {
 	rootCmd.AddCommand(syncCmd)
 	// syncCmd.PersistentFlags().String("foo", "", "A help for foo")
 	// syncCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
+
+func cloudBranchExists() bool {
+	msg, err := exec.Command("git", "pull").CombinedOutput()
+	if err != nil {
+		fmt.Println(err)
+	}
+	//log.Println("msg:", string(msg))
+	//log.Println("err:", err)
+	return !strings.Contains(string(msg), "There is no tracking information for the current branch")
+}
+
+func currentBranch() string {
+	msg, err := exec.Command("git", "branch", "--show-current").CombinedOutput()
+	if err != nil {
+		fmt.Println(err)
+	}
+	return strings.TrimSpace(string(msg))
 }
