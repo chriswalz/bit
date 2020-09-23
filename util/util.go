42facc42748f78650a7afe764e0a55e091df77c5
chriswalz <zlaw777@gmail.com>
2020-09-22 23:55:26 -0400

    Release /tmp/bit/git-extras/git-release.sh

--- util/util.go
+++ util/util.go
@@ -320,7 +320,7 @@ func RunScriptWithString(path string, script string, args ...string) {
 	}
 	f.WriteString(script)
 	defer f.Close()
-	//args = append([]string{path}, args...)
+	args = append([]string{path}, args...)
 	out, err := exec.Command(path, args...).Output()
 	if err != nil {
 		log.Fatal(err)
