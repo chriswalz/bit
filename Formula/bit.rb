af541a63f0136e4aaa7a5d9815685dace15d255e
goreleaserbot <walz@reconbuddy.com>
2020-10-15 23:53:05 -0400

    Brew formula update for bit version v0.6.6

--- Formula/bit.rb
+++ Formula/bit.rb
@@ -3,28 +3,21 @@ require_relative "custom_download_strategy"
 class Bit < Formula
   desc "A modern git CLI"
   homepage "https://github.com/chriswalz/bit"
-  version "0.6.5"
+  version "0.6.6"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/chriswalz/bit/releases/v0.6.5/bit_0.6.5_Darwin_x86_64.tar.gz", :using => CurlDownloadStrategy.
-    sha256 "2cd398486fa9f8808899d7411c2e0b1328432a5ce54b416ea357e803a14f5e48"
+    url "https://github.com/chriswalz/bit/releases/v0.6.6/bit_0.6.6_Darwin_x86_64.tar.gz", :using => CurlDownloadStrategy.
+    sha256 "908b98aafbc258b6bb0a9d5151af2c748e7ab7e4418340e9c025d51fa01c0073"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/chriswalz/bit/releases/v0.6.5/bit_0.6.5_Linux_x86_64.tar.gz", :using => CurlDownloadStrategy.
-      sha256 "056f179220d90b34033ffc1cce081ace1ea3d1d6af31e5bcd9eff84b3005a724"
+      url "https://github.com/chriswalz/bit/releases/v0.6.6/bit_0.6.6_Linux_x86_64.tar.gz", :using => CurlDownloadStrategy.
+      sha256 "dcdd97a104ac65290a64300044949cac7ee646d7a0bd257d7a9a6c6d12a00361"
     end
   end
-  
-  depends_on "git"
 
   def install
     bin.install "bit"
-  end
-
-  def caveats; <<~EOS
-    How to use this binary
-  EOS
   end
 
   test do
