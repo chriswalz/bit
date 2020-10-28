2e0a6056c70f026587a09eea3f21317b70f2ee2b
chriswalz <zlaw777@gmail.com>
2020-10-28 11:10:32 -0400

    enhancement: add sync upstream scaffolding

--- cmd/sync.go
+++ cmd/sync.go
@@ -16,16 +16,26 @@ sync origin master
 sync local-branch
 `,
 	Run: func(cmd *cobra.Command, args []string) {
-		RunInTerminalWithColor("git", []string{"fetch"})
+		upstream := "origin"
+		currentBranch := CurrentBranch()
+		tbranch := currentBranch
+		if len(args) == 1 {
+			tbranch = args[0]
+		}
+		if len(args) == 2 {
+			upstream = args[0]
+			tbranch = args[1]
+		}
+		RunInTerminalWithColor("git", []string{"fetch", upstream, currentBranch})
 
-		// if possibly squashed
+		// if possibly squashed or rebase needed
 		if IsDiverged() {
 			RunInTerminalWithColor("git", []string{"status", "-sb", "--untracked-files=no"})
 
 			ans := ""
 			optionMap := map[string]string{
 				"rebase": "Rebase on origin/upstream",
-				"force": "Force (destructive) push to origin/" + CurrentBranch(),
+				"force": "Force (destructive) push to " + upstream + "/" + currentBranch,
 				"cancel": "Cancel",
 			}
 			prompt := &survey.Select{
@@ -39,10 +49,10 @@ sync local-branch
 			fmt.Println()
 			survey.AskOne(prompt, &ans)
 			if ans == optionMap["rebase"] {
-				RunInTerminalWithColor("git", []string{"pull", "-r"})
+				RunInTerminalWithColor("git", []string{"pull", "-r", upstream, currentBranch})
 				return
 			} else if ans == optionMap["force"] {
-				RunInTerminalWithColor("git", []string{"push", "--force-with-lease"})
+				RunInTerminalWithColor("git", []string{"push", upstream, currentBranch, "--force-with-lease"})
 				// dont return user may have additional changes to save
 			} else {
 				fmt.Println("Canceling...")
@@ -50,44 +60,41 @@ sync local-branch
 			}
 		}
 		if !CloudBranchExists() {
-			RunInTerminalWithColor("git", []string{"push", "--set-upstream", "origin", CurrentBranch()})
+			RunInTerminalWithColor("git", []string{"push", "--set-upstream", upstream, currentBranch})
 			save("")
-			RunInTerminalWithColor("git", []string{"push"})
+			RunInTerminalWithColor("git", []string{"push", upstream, currentBranch})
 			return
 		}
 		save("")
-		if !CloudBranchExists() {
-			RunInTerminalWithColor("git", []string{"push", "--set-upstream", "origin", CurrentBranch()})
-		}
 		if IsAheadOfCurrent() {
-			RunInTerminalWithColor("git", []string{"push"})
+			RunInTerminalWithColor("git", []string{"push", upstream, currentBranch})
 		} else {
-			RunInTerminalWithColor("git", []string{"pull", "-r"})
-			if len(args) > 0 {
-				RunInTerminalWithColor("git", append([]string{"pull", "-r"}, args...))
-			}
-			RunInTerminalWithColor("git", []string{"push"})
+			RunInTerminalWithColor("git", []string{"pull", "-r", upstream, currentBranch})
+			RunInTerminalWithColor("git", []string{"push", upstream, currentBranch})
 		}
 
-		// After syncing with current branch and user wants to sync with another branch
+		// After syncing with current branch and user wants to sync with another tbranch
 
-		if CurrentBranch() == "master" && len(args) == 1 && strings.HasSuffix(args[0], "master") {
-			yes := AskConfirm("Squash & merge this branch into master?")
+		if CurrentBranch() == "master" && !strings.HasSuffix(tbranch, "master") {
+			yes := AskConfirm("Squash & merge " + args[0] +" into master?")
 
 			if yes {
-				RunInTerminalWithColor("git", []string{"merge", "--squash"})
+				RunInTerminalWithColor("git", []string{"merge", upstream, tbranch, "--squash"})
 				return
 			}
-			fmt.Printf("Cancelling...")
+			fmt.Println("Cancelling...")
 			//RunInTerminalWithColor("git", []string{"stash", "pop"}) deprecated switch stashing
 			return
 		}
+
+		//TODO sync with another branch when not on master
+		//
+		//
 
 		if len(args) == 1 {
 			branch := args[0]
 			refreshOnBranch(branch)
 		}
-
 	},
 	//Args: cobra.MaximumNArgs(1),
 }
